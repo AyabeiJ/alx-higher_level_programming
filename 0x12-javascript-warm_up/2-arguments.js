91a9342b9c98cf4f243a2320eb94b5e18779e735
AyabeiJ <ayabeihilda18@gmail.com>
2023-06-21 03:20:07 -0700

    warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-/*
-This script prints a message depending on the number of arguments passed.
-*/
 
-const args = process.argv.slice(2);
-const numArgs = args.length;
-
-if (numArgs === 0) {
-  console.log("No argument");
-} else if (numArgs === 1) {
-  console.log("Argument found");
+if (!process.argv[2]) {
+  console.log('No argument');
+} else if ((process.argv[2]) && (!process.argv[3])) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
